4098c2d346d0f9bf45737369a2267a9234e252a2
Harry <harrynp12@gmail.com>
2019-01-30 13:58:08 -0800

    initial random ai making valid moves

--- src/connect-k-python/StudentAI.py
+++ src/connect-k-python/StudentAI.py
@@ -1,13 +1,23 @@
-from random import randint
-from BoardClasses import Move
-from BoardClasses import Board
+from random import choice
+from BoardClasses import Board, Move
+from typing import List
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+
+
+class BoardWithAnalysis:
+    def __init__(self, board: Board, move: (int, int), heuristic: int) -> None:
+        self.board = board
+        self.move = move
+        self.heuristic = heuristic
+
+
 class StudentAI():
     col = 0
     row = 0
     k = 0
     g = 0
+    player_number = 2
 
     def __init__(self,col,row,k,g):
         self.g = g
@@ -17,8 +27,40 @@ class StudentAI():
         self.board = Board(col,row,k,g)
 
     def get_move(self,move):
-        self.board.make_move(move,1)
-        if self.g == 0:
-            return Move(randint(0,self.col-1),randint(0,self.row-1))
+        if move.col == -1 and move.row == -1:
+            self.player_number = 1
         else:
-            return Move(randint(0,self.col-1),0)
+            if self.player_number == 2:
+                self.board = self.board.make_move(move, 1)
+            else:
+                self.board = self.board.make_move(move, 2)
+        my_move = self.iterative_deepening(0, self.board, 0, 0, 0)
+        self.board = self.board.make_move(my_move, self.player_number)
+        return my_move
+
+    def iterative_deepening(self, depth: int, board: Board, turn: int) -> Move:
+        valid_moves = self.get_valid_moves(self.board)
+        my_move = choice(valid_moves)
+        return Move(my_move[0], my_move[1])
+
+    def alpha_beta_minmax(self, depth: int, board: Board, turn: int, alpha: int, beta: int) -> Move:
+        return True
+
+    def get_valid_moves(self, board: Board) -> List:
+        valid_moves = []
+        if self.g == 0:
+            for i in range(self.col):
+                for j in range(self.row):
+                    if board.is_valid_move(i, j):
+                        valid_moves.append((i, j))
+        else:
+            for i in range(self.col):
+                if board.board[0][i] == 0:
+                    valid_moves.append((i, 0))
+        return valid_moves
+
+    def expand_node(self, board: Board, move: (int, int)) -> List:
+        return self.get_valid_moves(board)
+
+    def goal_test(self, board: Board) -> int:
+        return board.is_win()
--- src/connect-k-python/main.py
+++ src/connect-k-python/main.py
@@ -2,18 +2,24 @@ from GameLogic import GameLogic
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
-        print("Invalid Parameters")
-        sys.exit(-1)
-
-    col = int(sys.argv[1])
-    row = int(sys.argv[2])
-    k = int(sys.argv[3])
-    g = int(sys.argv[4])
-    mode = sys.argv[5]
+    # if len(sys.argv) < 6:
+    #     print("Invalid Parameters")
+    #     sys.exit(-1)
+    #
+    # col = int(sys.argv[1])
+    # row = int(sys.argv[2])
+    # k = int(sys.argv[3])
+    # g = int(sys.argv[4])
+    # mode = sys.argv[5]
+    # debug = False
+    # if len(sys.argv) == 7 and sys.argv[6] == "-d":
+    #     debug = True
+    col = 4
+    row = 4
+    k = 4
+    g = 0
+    mode = "m"
     debug = False
-    if len(sys.argv) == 7 and sys.argv[6] == "-d":
-        debug = True
 
     main = GameLogic(col,row,k,g,mode,debug)
     main.Run()
