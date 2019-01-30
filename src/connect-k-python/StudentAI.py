8a45f9f0da738a60d9022c75cef09579feae5667
Harry <harrynp12@gmail.com>
2019-01-30 14:38:12 -0800

    changed get valid moves to generate game states instead

--- src/connect-k-python/StudentAI.py
+++ src/connect-k-python/StudentAI.py
@@ -1,12 +1,13 @@
 from random import choice
 from BoardClasses import Board, Move
 from typing import List
+import copy
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 
 
 class BoardWithAnalysis:
-    def __init__(self, board: Board, move: (int, int), heuristic: int) -> None:
+    def __init__(self, board: Board, move: Move, heuristic: int) -> None:
         self.board = board
         self.move = move
         self.heuristic = heuristic
@@ -18,6 +19,7 @@ class StudentAI():
     k = 0
     g = 0
     player_number = 2
+    opponent_number = 1
 
     def __init__(self,col,row,k,g):
         self.g = g
@@ -29,38 +31,46 @@ class StudentAI():
     def get_move(self,move):
         if move.col == -1 and move.row == -1:
             self.player_number = 1
+            self.opponent_number = 2
         else:
-            if self.player_number == 2:
-                self.board = self.board.make_move(move, 1)
-            else:
-                self.board = self.board.make_move(move, 2)
-        my_move = self.iterative_deepening(0, self.board, 0, 0, 0)
+            self.board = self.board.make_move(move, self.opponent_number)
+        my_move = self.iterative_deepening(0, self.board, 0)
         self.board = self.board.make_move(my_move, self.player_number)
         return my_move
 
     def iterative_deepening(self, depth: int, board: Board, turn: int) -> Move:
-        valid_moves = self.get_valid_moves(self.board)
-        my_move = choice(valid_moves)
-        return Move(my_move[0], my_move[1])
+        children = self.expand_node(self.board, 0)
+        random_state = choice(children)
+        return random_state.move
 
     def alpha_beta_minmax(self, depth: int, board: Board, turn: int, alpha: int, beta: int) -> Move:
         return True
 
-    def get_valid_moves(self, board: Board) -> List:
-        valid_moves = []
+    def expand_node(self, board: Board, turn: int) -> List:
+        children = []
         if self.g == 0:
             for i in range(self.col):
                 for j in range(self.row):
                     if board.is_valid_move(i, j):
-                        valid_moves.append((i, j))
+                        valid_move = Move(i, j)
+                        result_board = copy.deepcopy(board)
+                        if turn % 2 == 0:
+                            result_board = result_board.make_move(valid_move, self.player_number)
+                        else:
+                            result_board = result_board.make_move(valid_move, self.opponent_number)
+                        children.append(BoardWithAnalysis(result_board, valid_move, 0))
         else:
             for i in range(self.col):
                 if board.board[0][i] == 0:
-                    valid_moves.append((i, 0))
-        return valid_moves
+                    valid_move = Move(i, 0)
+                    result_board = copy.deepcopy(board)
+                    if turn % 2 == 0:
+                        result_board = result_board.make_move(valid_move, self.player_number)
+                    else:
+                        result_board = result_board.make_move(valid_move, self.opponent_number)
+                    children.append(BoardWithAnalysis(result_board, valid_move, 0))
+        return children
 
-    def expand_node(self, board: Board, move: (int, int)) -> List:
-        return self.get_valid_moves(board)
 
     def goal_test(self, board: Board) -> int:
         return board.is_win()
