a1bc4963254b0c66c4c417ab5fd223d92b788341
Harry <harrynp12@gmail.com>
2019-02-05 16:07:01 -0800

    fixed main

--- src/connect-k-python/main.py
+++ src/connect-k-python/main.py
@@ -2,24 +2,24 @@ from GameLogic import GameLogic
 import sys
 
 if __name__ == "__main__":
-    # if len(sys.argv) < 6:
-    #     print("Invalid Parameters")
-    #     sys.exit(-1)
-    #
-    # col = int(sys.argv[1])
-    # row = int(sys.argv[2])
-    # k = int(sys.argv[3])
-    # g = int(sys.argv[4])
-    # mode = sys.argv[5]
-    # debug = False
-    # if len(sys.argv) == 7 and sys.argv[6] == "-d":
-    #     debug = True
-    col = 7
-    row = 7
-    k = 5
-    g = 1
-    mode = "m"
+    if len(sys.argv) < 6:
+        print("Invalid Parameters")
+        sys.exit(-1)
+    
+    col = int(sys.argv[1])
+    row = int(sys.argv[2])
+    k = int(sys.argv[3])
+    g = int(sys.argv[4])
+    mode = sys.argv[5]
     debug = False
+    if len(sys.argv) == 7 and sys.argv[6] == "-d":
+        debug = True
+    # col = 7
+    # row = 7
+    # k = 5
+    # g = 1
+    # mode = "m"
+    # debug = False
 
     main = GameLogic(col,row,k,g,mode,debug)
     main.Run()
